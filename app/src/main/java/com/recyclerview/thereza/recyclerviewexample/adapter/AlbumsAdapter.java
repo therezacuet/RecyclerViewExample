2718b0cb0b06ad310020f5399387fe941525d0cc
Rezaul Islam <rejaulcuet88@gmail.com>
2018-02-22 05:26:02 -0600

    Initial commit

---
+++ app/src/main/java/com/recyclerview/thereza/recyclerviewexample/adapter/AlbumsAdapter.java
@@ -0,0 +1,133 @@
+package com.recyclerview.thereza.recyclerviewexample.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.recyclerview.thereza.recyclerviewexample.R;
+import com.recyclerview.thereza.recyclerviewexample.model.Album;
+
+import java.util.List;
+
+/**
+ * Created by Rebel on 2/22/2018.
+ */
+
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Album> albumList;
+    private AlbumsAdapterListener listener;
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, count;
+        public ImageView thumbnail, overflow;
+        public CardView cardView;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            count = (TextView) view.findViewById(R.id.count);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            overflow = (ImageView) view.findViewById(R.id.overflow);
+            cardView = view.findViewById(R.id.card_view);
+        }
+    }
+
+
+    public AlbumsAdapter(Context mContext, List<Album> albumList, AlbumsAdapterListener listener) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+        this.listener = listener;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.album_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        Album album = albumList.get(position);
+        holder.title.setText(album.getName());
+        holder.count.setText(album.getNumOfSongs() + " songs");
+
+        // loading album cover using Glide library
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
+            }
+        });
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onCardSelected(position, holder.thumbnail);
+            }
+        });
+
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+    private void showPopupMenu(View view) {
+        // inflate menu
+        android.support.v7.widget.PopupMenu popup = new android.support.v7.widget.PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_album, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    /**
+     * Click listener for popup menu items
+     */
+    class MyMenuItemClickListener implements android.support.v7.widget.PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+
+    public interface AlbumsAdapterListener {
+        void onAddToFavoriteSelected(int position);
+
+        void onPlayNextSelected(int position);
+
+        void onCardSelected(int position, ImageView thumbnail);
+    }
+}
---
+++ app/src/main/java/com/recyclerview/thereza/recyclerviewexample/model/Album.java
@@ -0,0 +1,41 @@
+package com.recyclerview.thereza.recyclerviewexample.model;
+
+/**
+ * Created by Rebel on 2/21/2018.
+ */
+
+public class Album {
+    private String name;
+    private int numOfSongs;
+    private int thumbnail;
+
+    public Album(String name, int numOfSongs, int thumbnail){
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
